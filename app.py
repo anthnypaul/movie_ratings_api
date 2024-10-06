fe2dde1e03fc9e08e8e56c34c134a5cb9319f4db
jhernandez4 <jokhernandez120744@gmail.com>
2024-10-05 23:08:22 -0700

    Link MySQL DB from .env file

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from sqlalchemy.exc import OperationalError
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+db = SQLAlchemy(app)
+
+@app.route('/check_db')
+def check_db_connection():
+    try:
+        # Establish a connection and execute a simple query using the connection
+        with db.engine.connect() as connection:
+            connection.execute(text('SELECT 1'))
+        return "MySQL connection is working."
+    except OperationalError:
+        return "Failed to connect to MySQL."
 
 @app.route('/')
 def home():
