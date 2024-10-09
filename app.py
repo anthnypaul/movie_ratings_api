9ef72ea262b025c48112a67ab4303a27d2373b0d
jhernandez4 <jokhernandez120744@gmail.com>
2024-10-08 18:33:15 -0700

    Add foreign keys to tables

--- app.py
+++ app.py
@@ -18,19 +18,33 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     isAdmin = db.Column(db.Boolean, nullable=False, default=False)
+    ratings = db.relationship('Rating', back_populates='user')
+
+    def __repr__(self):
+        return f"<User {self.username}"
 
 class Movie(db.Model):
     __tablename__ = 'movies'
     movie_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     release_year = db.Column(db.Integer)
+    ratings = db.relationship('Rating', back_populates='movie')
+
+    def __repr__(self):
+        return f"<Movie {self.title}"
 
 class Rating(db.Model):
     __tablename__ = 'ratings'
     rating_id = db.Column(db.Integer, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Intger, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     rating = db.Column(db.Integer, nullable=False)
+
+    user = db.relationship('User', back_populates='ratings')
+    movie = db.relationship('Movie', back_populates='ratings')
+
+    def __repr__(self):
+        return f"<Rating {self.rating_id}"
     
 
 @app.route('/check_db')
