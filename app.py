83754954f417c5a63ef9462cac14eff8dc88db00
jhernandez4 <jokhernandez120744@gmail.com>
2024-10-08 18:03:57 -0700

    Create DB tables using Flask

--- app.py
+++ app.py
@@ -11,6 +11,28 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    isAdmin = db.Column(db.Boolean, nullable=False, default=False)
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    movie_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    release_year = db.Column(db.Integer)
+
+class Rating(db.Model):
+    __tablename__ = 'ratings'
+    rating_id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Intger, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    
+
 @app.route('/check_db')
 def check_db_connection():
     try:
