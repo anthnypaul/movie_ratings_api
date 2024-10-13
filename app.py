6f578afd264211a02d495560b2f5a34437e84bfd
jhernandez4 <jokhernandez120744@gmail.com>
2024-10-12 20:45:18 -0700

    Move database models to a separate file

--- app.py
+++ app.py
@@ -5,48 +5,14 @@ from sqlalchemy.exc import OperationalError
 
 import os
 from dotenv import load_dotenv
+from database import db
+
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    __tablename__ = 'users'
-    user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    isAdmin = db.Column(db.Boolean, nullable=False, default=False)
-    ratings = db.relationship('Rating', back_populates='user')
-
-    def __repr__(self):
-        return f"<User {self.username}"
-
-class Movie(db.Model):
-    __tablename__ = 'movies'
-    movie_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    release_year = db.Column(db.Integer)
-    ratings = db.relationship('Rating', back_populates='movie')
-
-    def __repr__(self):
-        return f"<Movie {self.title}"
-
-class Rating(db.Model):
-    __tablename__ = 'ratings'
-    rating_id = db.Column(db.Integer, primary_key=True)
-    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    rating = db.Column(db.Integer, nullable=False)
-
-    user = db.relationship('User', back_populates='ratings')
-    movie = db.relationship('Movie', back_populates='ratings')
-
-    def __repr__(self):
-        return f"<Rating {self.rating_id}"
-    
-
+db.init_app(app)
+   
 @app.route('/check_db')
 def check_db_connection():
     try:
