0e8494e8280b8209dc8ea45105154c74b6596760
anthnypaul <polbagabaldo@outlook.com>
2024-10-15 15:22:00 -0700

    updated password length to fit hashed password

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ class User(db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(512), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     isAdmin = db.Column(db.Boolean, nullable=False, default=False)
     ratings = db.relationship('Rating', back_populates='user')
